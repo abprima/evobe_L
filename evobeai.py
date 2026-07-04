0a2b951ff9996812a54aae3a6a8befd9fc7e7db3
abprima <abeprima87@gmail.com>
2026-07-04 18:27:04 +0700

    debug

--- evobeai.py
+++ evobeai.py
@@ -748,6 +748,21 @@ with tabs[1]:
             # Convert results to a format for plotting
             labels = list(results.keys())
             data = np.array(list(results.values()))
+            st.write("========== DEBUG ==========")
+            st.write("Active CPL")
+            st.write(active_cpl)
+
+            st.write("Results")
+            st.write(results)
+
+            st.write("Data Before Numpy")
+            st.write(data)
+
+            data = np.array(data)
+
+            st.write("Shape")
+            st.write(data.shape)
+
             data_cum = data.cumsum(axis=1)
 
             fig, ax = plt.subplots(figsize=(9, 5))
